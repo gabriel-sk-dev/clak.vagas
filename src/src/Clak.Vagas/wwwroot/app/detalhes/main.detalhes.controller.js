6fda9a1b0852797ebb12021ef91fc78ac557b45a
leopoldo <leopoldojunior_@outlook.com>
2017-05-23 21:03:48 -0300

    controllers update

--- src/src/Clak.Vagas/wwwroot/app/detalhes/main.detalhes.controller.js
+++ src/src/Clak.Vagas/wwwroot/app/detalhes/main.detalhes.controller.js
@@ -3,8 +3,55 @@
     .module('mainModule')
     .controller('mainDetalhesController', mainDetalhesController);
 
-    function mainDetalhesController() {
+    function mainDetalhesController($state, $http, $stateParams, localStorageService, $mdSidenav) {
+        var vm = this;
+        vm.abrirCurriculo = abrirCurriculo;
+        vm.vaga = {
+            "id": "",
+            "titulo": "",
+            "detalhes": "",
+            "salario": "",
+            "carga_horaria": "",
+            "tipo_contratacao": ""
+        };
+        vm.registro = {
+            "id_user": "",
+            "id_vaga": ""
+        }
+        ativar();
+        function ativar() {
+            $http
+                .get("http://localhost:5000/api/Vagas/" + $stateParams.id)
+                .then(
+                    function (result) {
+                        vm.vaga = result.data;
+                    },
+                    function (error) { }
+                );
+        }
+        function abrirCurriculo() {
+            var loginId = localStorageService.get('loginId');
+            console.log(loginId);
+            if (loginId === null)
+                $mdSidenav('right').toggle();
+            else {
+                vm.registro.id_user = loginId;
+                vm.registro.id_vaga = $stateParams.id;
+                $http
+                    .post("http://localhost:5000/api/Vagas/inscricao", vm.registro)
+                    .then(
+                        function (result) {
+                            alert("Currículo enviado!");
+                            vm.registro = result.data;
+                            $state.go('vagas');
+                        },
+                        function (error) {
+                            console.log(error);
+                            alert("Algo inesperado aconteceu. Tente novamente!");
+                        }
+                    );
+            }
 
+        }
     }
-
 })();
--- src/src/Clak.Vagas/wwwroot/app/vagas/main.vagas.controller.js
+++ src/src/Clak.Vagas/wwwroot/app/vagas/main.vagas.controller.js
@@ -3,8 +3,24 @@
     .module('mainModule')
     .controller('mainVagasController', mainVagasController);
 
-    function mainVagasController(){
+    function mainVagasController($state, $http){
+        var vm = this;
+        vm.vagas = [];
+        vm.abrirVaga = abrirVaga;
+        ativar();
+        function ativar() {
+            $http
+                .get("http://localhost:5000/api/Vagas")
+                .then(
+                    function (result) {
+                        vm.vagas = result.data;
+                    },
+                    function (error) { }
+                );
 
+        }
+        function abrirVaga(id) {
+            $state.go('vagaDetalhe', { id: id });
+        }
     }
-
 })();
