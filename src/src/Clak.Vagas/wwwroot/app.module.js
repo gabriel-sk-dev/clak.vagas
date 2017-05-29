7cc4c6ed1e47756715811b3be076a7e35538027b
leopoldo <leopoldojunior_@outlook.com>
2017-05-29 19:30:54 -0300

    main module

--- src/src/Clak.Vagas/wwwroot/app.module.js
+++ src/src/Clak.Vagas/wwwroot/app.module.js
@@ -32,6 +32,14 @@
                controllerAs: "vm"
            }
        );
+        $stateProvider.state('login',
+         {
+             url: '/login',
+             templateUrl: "app/login/main.login.view.html",
+             controller: "mainLoginController",
+             controllerAs: "vm"
+         }
+     );
     }
 })();
 
