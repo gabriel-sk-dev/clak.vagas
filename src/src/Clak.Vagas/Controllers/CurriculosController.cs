d833334d5ef7e9c42adb07e49c10c678945832af
leopoldo <leopoldojunior_@outlook.com>
2017-05-29 18:57:13 -0300

    debug

--- src/src/Clak.Vagas/Controllers/CurriculosController.cs
+++ src/src/Clak.Vagas/Controllers/CurriculosController.cs
@@ -18,25 +18,7 @@ namespace Clak.Vagas.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
+            return null;
         }
 
         // DELETE api/values/5
