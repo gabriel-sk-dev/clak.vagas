cada3f56ee56d331ba4b7c27497318b5c9f7de02
leopoldo <leopoldojunior_@outlook.com>
2017-05-30 21:17:09 -0300

    mudanças pagina adm

--- src/src/Clak.Vagas/wwwroot/app/admin/main.admin.controller.js
+++ src/src/Clak.Vagas/wwwroot/app/admin/main.admin.controller.js
@@ -1,10 +1,48 @@
 ﻿(function () {
     angular
-    .module('mainModule')
-    .controller('mainAdminController', mainAdminController);
+       .module('mainModule')
+       .controller('mainAdminController', mainAdminController);
 
-    function mainAdminController() {
-        
+    function mainAdminController(localStorageService, $state, $stateParams, $http) {
+
+        var vm = this;
+        vm.VerCandidato = VerCandidato;
+        vm.vagas = [
+            {
+                "id": 1,
+                "titulo": "vaga1",
+                "quantidade": 1
+            }
+        ]
+        ativar();
+
+        function ativar() {
+            var tipo = localStorageService.get('tipo');
+            var id = localStorageService.get('login');
+            if (id === null) {
+                $state.go('vagas', { vagaId: $stateParams.vagaId });
+                return;
+            }
+            if (tipo !== undefined && tipo !== "adm") {
+                $state.go('login');
+            }
+
+            $http
+                .get("http://localhost:5000/api/vagas/admin")
+                .then(
+                    function (result) {
+
+                        vm.vagas = result.data;
+
+                    },
+                    function (error) {
+
+                    }
+                )
+        }
+        function VerCandidato() {
+            $state.go('curriculos')
+        }
     }
-     
+
 })();
